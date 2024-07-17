0c5f586ed44df15570502d6cede212e3d9878049
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-17 19:59:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let myform = document.getElementById('imgurl');
+let targetImage = document.getElementById('targetimage');
+let inputRange = document.querySelectorAll('.slider')
+myform.addEventListener('submit', function(e) {
+  let urlImage = document.getElementById('onlineurl');
+  let urlImageval = urlImage.value;
+  if(urlImageval.length){
+    targetImage.setAttribute('src' ,urlImageval);
+      urlImage.vaule = '';
+  }
+  e.preventDefault();
+  console.log('Image displayed');
+});
+for (let i = 0; i < inputRange.length; i++) {
+    inputRange[i].addEventListener('input', editImage);
+}
+function editImage() {
+  let gs = document.getElementById('gs');
+  let blur = document.getElementById('blur');
+  let huerotate = document.getElementById('hue-rotate');
+  let sepia = document.getElementById('sepia');
+  let gsval = gs.value;
+  let blurval = blur.value;
+  let huerotateval = huerotate.value;
+  let sepiaval = sepia.value;
+  targetImage.style.filter = 'greyscale('+gsval+'%) blur('+blurval+'px) hue-rotate(' +huerotateval+ 'deg) sepia('+sepiaval+'%)';
+}
+let sliderForm = document.getElementById('slider-form');
+sliderForm.addEventListener('reset', function() {
+  sliderForm.reset();
+  setTimeout(function() {
+    editImage();
+  }, 0)
+})
